6d81759fd463bae4ca594176888f38bcfbe42c71
Donghyeon Kim <0916dhkim@gmail.com>
2020-07-11 19:40:12 +0900

    Add Spotify Credentials Configs

--- config.py
+++ config.py
@@ -8,4 +8,6 @@ load_dotenv()
 config = {
     "prefix": os.getenv("BOT_PREFIX"),
     "token": os.getenv("BOT_TOKEN"),
+    "spotifyClientId": os.getenv("SPOTIFY_CLIENT_ID"),
+    "spotifyClientSecret": os.getenv("SPOTIFY_CLIENT_SECRET")
 }
